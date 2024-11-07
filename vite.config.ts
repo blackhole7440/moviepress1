15ff68e62819964ab39fbda265f2a2cf22a28000
blackhole7440 <yourtriphotels@gmail.com>
2024-11-07 21:52:11 +0700

    update vts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://best.moviepress.top/',
+  base: '',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
